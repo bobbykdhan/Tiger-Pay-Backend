a8607443dae2736c45dfc1aca406fa43e1aa3f13
Bobby Dhanoolal <bobbydhanoolal@gmail.com>
2022-10-18 16:05:47 -0400

    Figured out how to crop the qr code and how to parse

--- main.py
+++ main.py
@@ -5,7 +5,23 @@
 
 
 def print_hi(name):
-    pass
+    from pylibdmtx.pylibdmtx import decode
+    from PIL import Image
+
+    im = Image.open(r"testlol.jpeg")
+
+    im1 = im.crop((250, 685, 451, 885))
+    im1.show()
+    print(decode(Image.open("final.png")))
+
+
+# Setting the points for cropped image
+# left = 250
+# top = height / 1.54
+# right = 451
+# bottom = 2.9 * height / 3.44
+#
+# print(str(left) + ", " + str(top) + ", " + str(right) + ", " + str(bottom) + ", ")
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
