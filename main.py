feb17bcdef05155be45387f82a5e266f5367efe9
Bobby Dhanoolal <bobbydhanoolal@gmail.com>
2022-10-19 18:46:10 -0400

    Started making the API

--- main.py
+++ main.py
@@ -1,30 +1,54 @@
-# This is a sample Python script.
+from fastapi import FastAPI
+from pylibdmtx.pylibdmtx import decode
+from PIL import Image
+from bs4 import BeautifulSoup as bs4
+import requests
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+app = FastAPI()
 
 
-def print_hi(name):
-    from pylibdmtx.pylibdmtx import decode
-    from PIL import Image
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
+
+@app.get("/card")
+async def read_item(skey: str = 0, cid: str = "105", side: str = ""):
     im = Image.open(r"testlol.jpeg")
 
     im1 = im.crop((250, 685, 451, 885))
-    im1.show()
+    # im1.show()
     print(decode(Image.open("final.png")))
 
+    payload = {
+        'skey': skey,
+        'cid': cid,
+        'side': "front"
+    }
 
-# Setting the points for cropped image
-# left = 250
-# top = height / 1.54
-# right = 451
-# bottom = 2.9 * height / 3.44
-#
-# print(str(left) + ", " + str(top) + ", " + str(right) + ", " + str(bottom) + ", ")
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    response = requests.get(
+        "https://tigerspend.rit.edu/virtualcardnew.php", payload).content
+    test = bs4(response, "html.parser")
+
+
+    if "Login" in test.text:
+        print("SKEY INVALID")
+        return
+
+    card = requests.get(
+        "https://tigerspend.rit.edu/" + 'imagestudentcard.php?cid=105&skey=' + payload["skey"] + '&side=front')
+
+    with open('card.jpg', 'wb') as handler:
+        handler.write(card.content)
+        im = Image.open(r"card.jpg")
+        im.show()
+        im1 = im.crop((250, 685, 451, 885))
+        im1.show()
+        im1.save("crop.png")
+        code = decode(Image.open("crop.png"))[0].data
+        print(code)
+        return {("Card code: "  +str(code) ) : ("Skey: " +skey)}
+
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
