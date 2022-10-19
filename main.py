5d7680fc5b1e4bce0564fbe246d1955a48c4b63b
Bobby Dhanoolal <bobbydhanoolal@gmail.com>
2022-10-19 19:00:04 -0400

    Refactoring

--- main.py
+++ main.py
@@ -14,11 +14,6 @@ def read_root():
 
 @app.get("/card")
 async def read_item(skey: str = 0, cid: str = "105", side: str = ""):
-    im = Image.open(r"testlol.jpeg")
-
-    im1 = im.crop((250, 685, 451, 885))
-    # im1.show()
-    print(decode(Image.open("final.png")))
 
     payload = {
         'skey': skey,
@@ -30,7 +25,6 @@ async def read_item(skey: str = 0, cid: str = "105", side: str = ""):
         "https://tigerspend.rit.edu/virtualcardnew.php", payload).content
     test = bs4(response, "html.parser")
 
-
     if "Login" in test.text:
         print("SKEY INVALID")
         return
@@ -41,14 +35,14 @@ async def read_item(skey: str = 0, cid: str = "105", side: str = ""):
     with open('card.jpg', 'wb') as handler:
         handler.write(card.content)
         im = Image.open(r"card.jpg")
-        im.show()
+
         im1 = im.crop((250, 685, 451, 885))
         im1.show()
         im1.save("crop.png")
         code = decode(Image.open("crop.png"))[0].data
+
+        im.show()
         print(code)
-        return {("Card code: "  +str(code) ) : ("Skey: " +skey)}
-
-
+        return {code: "World"}
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
